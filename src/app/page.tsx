4122bb3779287ae0ca9692c23490d2f7256f47a7
David Alvarez Barraza <david.alvarez.barraza@gmail.com>
2026-08-04 22:32:00 -0400

    feat: add product view bar chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { DashboardShell } from "@/components/layout/DashboardShell";
 import { OverviewCard } from "@/features/dashboard/components/OverviewCard";
+import { ProductViewChart } from "@/features/dashboard/components/ProductViewChart";
 
 export default function Home() {
   return (
     <DashboardShell>
-      <OverviewCard />
+      <div className="flex flex-col gap-6">
+        <OverviewCard />
+        <ProductViewChart />
+      </div>
     </DashboardShell>
   );
 }
